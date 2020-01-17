15d6d0b75ae221c2e6227ce61ba1d6f68043f8ef
anglihust <47904962+anglihust@users.noreply.github.com>
2020-01-17 16:39:50 -0500

    Delete other.h

--- other.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef OTHER_H
-#define OTHER_H
-using namespace std;
-int log2(int i);//log function
-int div2(int N);//N divsion by 2 
-void writefile(float *wfile, int size_n, char *save_path);
-#endif
